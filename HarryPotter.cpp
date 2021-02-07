73cf730b31f4015fbcf88cc7d7aa492418c0cc9d
Jonathan Tran <jonathan.t.a.tran@gmail.com>
2021-02-07 11:24:30 -0800

    Added files via upload

---
+++ HarryPotter.cpp
@@ -0,0 +1,11 @@
+// Harry Potter
+#include <iostream>
+
+int main()
+{
+  // Prints "Expecto Patronum" to the terminal
+  std::cout << "Expecto Patronum\n";
+  
+  return 0;
+  
+}
---
+++ initials.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  // This program prints out the initials JT
+  std::cout << "My initials are JT.\n\n";
+  std::cout << "JJJJJ TTTTT\n";
+  std::cout << "  J     T  \n";
+  std::cout << "  J     T  \n";
+  std::cout << "J J     T  \n";
+  std::cout << "J J     T  \n";
+  std::cout << " JJ     T  \n";
+
+}
