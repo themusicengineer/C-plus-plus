6d6407976011c166e8399409821ffcac6ed9f4b3
Jonathan Tran <jonathan.t.a.tran@gmail.com>
2021-02-07 18:43:46 -0800

    Update temperature.cpp
    Added a std::cin element to capture user input.

--- temperature.cpp
+++ temperature.cpp
@@ -5,11 +5,13 @@ int main() {
   
 // New York City 31 degrees F on 02/07/2021 at 06:20 PM
 
-double tempf = 31.0;
+double tempf;
 double tempc;  
-  
-tempc = (tempf - 32)/1.8;
 
+cout << "This program converts the temperature in Fahrenheit to Celsius.\n";
+cout << "What is the temperature in Fahrenheit?\n";
+cin >> tempf;
+tempc = (tempf - 32)/1.8;
 cout << "The temp is " << tempc << " degrees Celsius.\n";
   
 }
