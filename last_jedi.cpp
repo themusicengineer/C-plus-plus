ffc8172c3423f933a2d3d6ea7d69ed964527991f
Jonathan Tran <jonathan.t.a.tran@gmail.com>
2021-02-16 14:21:43 -0800

    Added last_jedi.cpp to C++
    Uses .push_back function to add vector elements.

---
+++ last_jedi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  
+  std::vector<std::string> last_jedi;
+  
+  // Add characters here:
+  last_jedi.push_back("kylo");
+  last_jedi.push_back("rey");
+  last_jedi.push_back("luke");
+  last_jedi.push_back("finn");
+  
+  cout << last_jedi[0] << " ";
+  cout << last_jedi[1] << " ";
+  cout << last_jedi[2] << " ";
+  cout << last_jedi[3] << " ";
+  
+}
